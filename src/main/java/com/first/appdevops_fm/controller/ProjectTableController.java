47dffb1174d77057be21cb5d84da4e8a2ca09ba5
Madhavan1232 <madhavanvairavan3@gmail.com>
2026-01-19 20:17:11 +0530

    final

--- src/main/java/com/first/appdevops_fm/controller/ProjectTableController.java
+++ src/main/java/com/first/appdevops_fm/controller/ProjectTableController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/projects-tables")
 @CrossOrigin(origins = "*")
 public class ProjectTableController {
 
